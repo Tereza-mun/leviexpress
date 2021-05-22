a4d42f8b8dffceddaede9e6953f614ad319d70c9
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2021-05-22 14:43:42 +0200

    useState added

--- src/Home/Journey picker/index.jsx
+++ src/Home/Journey picker/index.jsx
@@ -1,7 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './style.css';
 import mapImage from './img/map.svg';
 
+const CityOptions = ({ cities }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {cities.map((city) => (
+        <option key={city.code} value={city.code}>
+          {city.name}
+        </option>
+      ))}
+      ;
+    </>
+  );
+};
+
 const JourneyPicker = () => {
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
+  const [date, setDate] = useState('');
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    setCities([
+      { name: 'Praha', code: 'CZ-PRG' },
+      { name: 'Brno', code: 'CZ-BRQ' },
+    ]);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Odesilam formular s cestou');
+    console.log(fromCity);
+    console.log(toCity);
+    console.log(date);
+  };
+
   return (
     <>
       <div className="journey-picker container">
@@ -10,7 +45,10 @@ const JourneyPicker = () => {
           <form className="journey-picker__form">
             <label>
               <div className="journey-picker__label">Odkud:</div>
-              <select>
+              <select
+                value={fromCity}
+                onChange={(e) => setFromCity(e.target.value)}
+              >
                 <option value="">Vyberte</option>
                 <option value="Mesto1">Město 1</option>
                 <option value="Mesto2">Město 2</option>
@@ -20,7 +58,10 @@ const JourneyPicker = () => {
             </label>
             <label>
               <div className="journey-picker__label">Kam:</div>
-              <select>
+              <select
+                value={toCity}
+                onChange={(e) => setToCity(e.target.value)}
+              >
                 <option value="">Vyberte</option>
                 <option value="Mesto1">Město 1</option>
                 <option value="Mesto2">Město 2</option>
@@ -30,7 +71,7 @@ const JourneyPicker = () => {
             </label>
             <label>
               <div className="journey-picker__label">Datum:</div>
-              <select>
+              <select value={date} onChange={(e) => setDate(e.target.value)}>
                 <option value="">Vyberte</option>
                 <option>20.05.2021</option>
                 <option>21.05.2021</option>
@@ -39,7 +80,7 @@ const JourneyPicker = () => {
               </select>
             </label>
             <div className="journey-picker__controls">
-              <button className="btn" type="submit">
+              <button onClick={handleSubmit} className="btn" type="submit">
                 Vyhledat spoj
               </button>
             </div>
@@ -50,12 +91,5 @@ const JourneyPicker = () => {
     </>
   );
 };
-
-// const JourneyPicker = () => {
-//   const [fromCity, setFromCity] = useState('');
-//   const [toCity, setToCity] = useState('');
-//   const [date, setDate] = useState('');
-//   const [cities, setToCities] = useState([]);
-// };
 
 export default JourneyPicker;
