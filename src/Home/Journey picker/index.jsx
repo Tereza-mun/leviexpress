249b4e58c695f7dfed02a5cb17d5fe260839c805
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2021-05-22 17:06:50 +0200

    API added

--- src/Home/Journey picker/index.jsx
+++ src/Home/Journey picker/index.jsx
@@ -16,17 +16,34 @@ const CityOptions = ({ cities }) => {
   );
 };
 
+const DatesOptions = ({ dates }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {dates.map((date) => {
+        <option key={date}>{date}</option>;
+      })}
+    </>
+  );
+};
+
 const JourneyPicker = () => {
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
   const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
 
   useEffect(() => {
-    setCities([
-      { name: 'Praha', code: 'CZ-PRG' },
-      { name: 'Brno', code: 'CZ-BRQ' },
-    ]);
+    fetch('https://leviexpress-backend.herokuapp.com/api/cities')
+      .then((resp) => resp.json())
+      .then((json) => console.log(json.data));
+  }, []);
+
+  useEffect(() => {
+    fetch('https://leviexpress-backend.herokuapp.com/api/dates')
+      .then((resp) => resp.json())
+      .then((json) => console.log(json.data));
   }, []);
 
   const handleSubmit = (e) => {
@@ -35,6 +52,23 @@ const JourneyPicker = () => {
     console.log(fromCity);
     console.log(toCity);
     console.log(date);
+    fetch(
+      `https://leviexpress-backend.herokuapp.com/api/journey?fromCity=${fromCity}&toCity=${toCity}&date=${date}`,
+    )
+      .then((resp) => resp.json())
+      .then((json) => console.log(json.data));
+  };
+
+  const handleFromCityChange = (e) => {
+    setFromCity(e.target.value);
+  };
+
+  const handleToCityChange = (e) => {
+    setToCity(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
   };
 
   return (
@@ -45,40 +79,25 @@ const JourneyPicker = () => {
           <form className="journey-picker__form">
             <label>
               <div className="journey-picker__label">Odkud:</div>
-              <select
-                value={fromCity}
-                onChange={(e) => setFromCity(e.target.value)}
-              >
-                <option value="">Vyberte</option>
-                <option value="Mesto1">Město 1</option>
-                <option value="Mesto2">Město 2</option>
-                <option value="Mesto3">Město 3</option>
-                <option value="Mesto4">Město 4</option>
+              <select value={fromCity} onChange={handleFromCityChange}>
+                <CityOptions cities={cities} />
               </select>
             </label>
+
             <label>
               <div className="journey-picker__label">Kam:</div>
-              <select
-                value={toCity}
-                onChange={(e) => setToCity(e.target.value)}
-              >
-                <option value="">Vyberte</option>
-                <option value="Mesto1">Město 1</option>
-                <option value="Mesto2">Město 2</option>
-                <option value="Mesto3">Město 3</option>
-                <option value="Mesto4">Město 4</option>
+              <select value={toCity} onChange={handleToCityChange}>
+                <CityOptions cities={cities} />
               </select>
             </label>
+
             <label>
               <div className="journey-picker__label">Datum:</div>
-              <select value={date} onChange={(e) => setDate(e.target.value)}>
-                <option value="">Vyberte</option>
-                <option>20.05.2021</option>
-                <option>21.05.2021</option>
-                <option>22.05.2021</option>
-                <option>23.05.2021</option>
+              <select value={date} onChange={handleDateChange}>
+                <DatesOptions dates={dates} />
               </select>
             </label>
+
             <div className="journey-picker__controls">
               <button onClick={handleSubmit} className="btn" type="submit">
                 Vyhledat spoj
