0fd4b234ebacf25f2440afcc3107ce70f6d4f1c4
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2021-06-22 10:11:12 +0200

    history push fixed

--- src/Reservation/index.jsx
+++ src/Reservation/index.jsx
@@ -4,45 +4,45 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Reservation = () => {
-  const [reservation, setReservation] = useState(null);
+  // const [reservation, setReservation] = useState(null);
 
-  const { id } = useParams();
+  // const { id } = useParams();
 
-  useEffect(() => {
-    fetch(`https://leviexpress-backend.herokuapp.com/api/reservation?id=${id}`)
-      .then((resp) => resp.json())
-      .then((json) => setReservation(json.data));
-  }, []);
+  // useEffect(() => {
+  //   fetch(`https://leviexpress-backend.herokuapp.com/api/reservation?id=${id}`)
+  //     .then((resp) => resp.json())
+  //     .then((json) => setReservation(json.data));
+  // }, []);
 
   return (
     <>
-      {reservation && (
-        <div className="reservation">
-          <h2>Vaše e-jízdenka</h2>
-          <div className="reservation__body">
-            <div className="reservation__body__headings">
-              <p>Datum cesty:</p>
-              <p>Z:</p>
-              <p>Do:</p>
-              <p>Sedadlo:</p>
-            </div>
-            <div className="reservation__body__data">
-              <p>
-                <strong>22.6.2021</strong>
-              </p>
-              <p>
-                <strong>Brno</strong>
-              </p>
-              <p>
-                <strong>Eindhoven</strong>
-              </p>
-              <p>
-                <strong>3</strong>
-              </p>
-            </div>
+      {/* {reservation && ( */}
+      <div className="reservation">
+        <h2>Vaše e-jízdenka</h2>
+        <div className="reservation__body">
+          <div className="reservation__body__headings">
+            <p>Datum cesty:</p>
+            <p>Z:</p>
+            <p>Do:</p>
+            <p>Sedadlo:</p>
+          </div>
+          <div className="reservation__body__data">
+            <p>
+              <strong>22.6.2021</strong>
+            </p>
+            <p>
+              <strong>Brno</strong>
+            </p>
+            <p>
+              <strong>Eindhoven</strong>
+            </p>
+            <p>
+              <strong>3</strong>
+            </p>
           </div>
         </div>
-      )}
+      </div>
+      ){/* } */}
     </>
   );
 };
